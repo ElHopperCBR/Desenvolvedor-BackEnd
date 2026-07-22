bfcb83cac25932f918de735809e793f4098bcd91
engPlatini <platiniluiz@gmail.com>
2026-07-22 19:47:19 -0300

    Operadores lógicos e relacionais

---
+++ Lógica de programação e algoritmo/Aula03/Operadores/Operadores/Program.cs
@@ -0,0 +1,38 @@
+﻿//Operadores relacionais
+/*  <  | menor
+ *  >  | maior
+ *  <= | menor ou igual
+ *  >= | maior ou igual
+ *  == | igual
+ *  != | diferente 
+ */
+/*
+double x = 2.14;
+double y = 2;
+Console.WriteLine(x < y);
+Console.WriteLine(x <= y);
+Console.WriteLine(x > y);
+Console.WriteLine(x >= y);
+
+char a = 'S';
+char b = 'N';
+Console.WriteLine(a == b);
+Console.WriteLine(a != b);
+
+Console.WriteLine(a == b ? "Verdade" : "OK");
+*/
+//Operadores Lógicos
+/*
+ *  !  | Negação lógica
+ *  &  | E lógico
+ *  |  | OU lógico
+ *  ^  | OU exclusiva lógica
+ *  && | E lógico condicional
+ *  || | OU lógico condicional
+ * 
+*/
+
+bool x = false;
+bool y = false;
+
+Console.WriteLine(x || y);
