adc83c519cd8d4c7f13565326d042f73eb1ec693
engPlatini <platiniluiz@gmail.com>
2026-08-07 21:54:26 -0300

    Finalizado logica de programação no backend

---
+++ Lógica de programação e algoritmo/Aula09/senai.cs
@@ -0,0 +1,15 @@
+System.Console.WriteLine("""
+
+========================================
+           CARDÁPIO DA LANCHONETE       
+========================================
+CÓDIGO | ITEM            | PREÇO       
+----------------------------------------
+  1    | Cachorro Quente | R$ 4,50     
+  2    | X-Salada        | R$ 4,50     
+  3    | X-Bacon         | R$ 5,00     
+  4    | Torrada Simples | R$ 2,00     
+  5    | Refrigerante    | R$ 1,50     
+========================================
+
+""");
