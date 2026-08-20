b9a5360e678a7cc55ccc4920ea573f6351eebba2
engPlatini <platiniluiz@gmail.com>
2026-08-19 21:16:06 -0300

    Relação entre objetos agregação e composição

---
+++ Programação orientada a objetos/Aula05/Quarta/Pedido/Classes/Entidades/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿namespace Pedido.Classes.Entidades
+{
+    //Objeto que pode exisitr de forma independente
+    internal class Funcionario
+    {
+        //1° - Campos
+        private string nome;
+
+        //2° - Propriedades 
+        protected string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        //3° - Construtor
+        public Funcionario(string nome)
+        {
+            Nome = nome;
+        }
+
+    }
+}
