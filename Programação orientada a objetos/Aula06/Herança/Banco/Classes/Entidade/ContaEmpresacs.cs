927420520c0034d4d2bc745a84a32e1244f6a183
engPlatini <platiniluiz@gmail.com>
2026-08-24 21:54:12 -0300

    feito multiplas interfaces com herança

---
+++ Programação orientada a objetos/Aula06/Herança/Banco/Classes/Entidade/ContaEmpresacs.cs
@@ -0,0 +1,44 @@
+﻿namespace Banco.Classes.Entidade
+{
+    internal class ContaEmpresacs : Conta
+    {
+		//Campos
+		private double limite;
+
+
+        //Propriedades
+        protected double LimiteDeEmprestimo
+		{
+			get { return limite; }
+			set { limite = value; }
+		}
+		//Construtores
+        public ContaEmpresacs(int numeroDaConta, string titularDaConta, double limite) : base(numeroDaConta, titularDaConta)
+        {
+            LimiteDeEmprestimo = limite;
+        }
+
+        public ContaEmpresacs(int numeroDaConta, string titularDaConta, double saldoDaConta, double limite) : base(numeroDaConta, titularDaConta, saldoDaConta)
+        {
+            LimiteDeEmprestimo = limite;
+        }
+
+        //Metodos
+        public override string ToString()
+        {
+            return $"Dados da conta:\n" +
+                $"\tTitular: {TitularDaConta}\n" +
+                $"\tNúmero: {NumeroDaConta}\n" +
+                $"\tSaldo: {SaldoDaConta:c}\n" +
+                $"\tLimite: {LimiteDeEmprestimo:c}\n";
+        }
+
+        public void Emprestimo(double quantia)
+        {
+            LimiteDeEmprestimo -= quantia;
+            SaldoDaConta += quantia;
+        }
+
+
+	}
+}
