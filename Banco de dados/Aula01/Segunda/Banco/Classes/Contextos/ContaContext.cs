6b8efae788093dd0a925ddb7fb577e10e9eb935e
engPlatini <platiniluiz@gmail.com>
2026-08-31 21:54:13 -0300

    Primeira aula de banco de dados

---
+++ Banco de dados/Aula01/Segunda/Banco/Classes/Contextos/ContaContext.cs
@@ -0,0 +1,29 @@
+﻿using Banco.Classes.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banco.Classes.Contextos
+{
+    internal class ContaContext : DbContext
+    {
+        //Propriedade - Representa as contas no BD
+        public DbSet<Conta> contas { get; set; }
+
+        //Métodos
+        protected override void OnConfiguring(DbContextOptionsBuilder opcoesDeContrucao)
+        {
+            string config = @"Server=(localdb)\MSSQLLocalDB;Database=BancoDBContas;Trusted_Connection=True;TrustServerCertificate=True;";
+            opcoesDeContrucao.UseSqlServer(config);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modeloDeConstrucao)
+        {
+            modeloDeConstrucao.Entity<Conta>(entidade =>
+            {
+                entidade.HasKey(e => e.Id );
+                entidade.Property(e => e.NumeroDaConta);
+                entidade.Property(e => e.TitularDaConta);
+                entidade.Property(e => e.SaldoDaConta);
+            });
+        }
+    }
+}
